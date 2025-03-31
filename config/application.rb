3242c2761f1ad610e454c326ea571acb5d284a4c
naoisobe <62488600+naoisobe@users.noreply.github.com>
2025-03-31 16:10:01 +0900

    Add views for Posts, Home pages and configuration files

---
+++ config/application.rb
@@ -0,0 +1,40 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module McpTest
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    
+    # Set default locale
+    config.i18n.default_locale = :en
+    
+    # Set time zone
+    config.time_zone = 'Tokyo'
+    
+    # Configure generators
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+  end
+end
